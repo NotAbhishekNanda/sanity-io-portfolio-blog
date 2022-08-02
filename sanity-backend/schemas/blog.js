ff395b5d811ff47479e6e9e90f888d1c4d5877b3
abhisheknanda65-zessta <abhishek.nanda@zessta.com>
2022-08-02 17:22:35 +0530

    connecting sanity backend withnext front-end

--- sanity-backend/schemas/blog.js
+++ sanity-backend/schemas/blog.js
@@ -3,85 +3,97 @@ export default {
   type: 'document',
   title: 'Blog',
   fields: [
-    {
-      name: 'title',
-      type: 'string',
-      title: 'Title',
-    },
-    {
-      name: 'content',
-      title: 'Content',
-      type: 'array',
-      of: [
-        {
-          type: 'block'
+      {
+          name: 'title',
+          type: 'string',
+          title: 'Title'
+      },
+      {
+        title: 'Slug',
+        name: 'slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+          maxLength: 200, // will be ignored if slugify is set
+          slugify: input => input
+                               .toLowerCase()
+                               .replace(/\s+/g, '-')
+                               .slice(0, 200)
+        }
+      },
+      {
+          name: 'content',
+          title: 'Content',
+          type: 'array',
+          of: [
+            {
+              type: 'block'
+            },
+            {
+              type: 'image',
+              fields: [
+                {
+                  type: 'text',
+                  name: 'alt',
+                  title: 'Alternative text',
+                  description: `Some of your visitors cannot see images, 
+                    be they blind, color-blind, low-sighted; 
+                    alternative text is of great help for those 
+                    people that can rely on it to have a good idea of 
+                    what\'s on your page.`,
+                  options: {
+                    isHighlighted: true
+                  }
+                }
+              ]
+            }
+          ]
         },
         {
+          name: 'metadesc',
+          type: 'string',
+          title: 'Meta Description'
+      },
+        {
+          title: 'Blog Image',
+          name: 'blogimage',
           type: 'image',
+          options: {
+            hotspot: true // <-- Defaults to false
+          },
           fields: [
             {
-              type: 'text',
-              name: 'alt',
-              title: 'Alternative text',
-              description: `Some of your visitors cannot see images, 
-                be they blind, color-blind, low-sighted; 
-                alternative text is of great help for those 
-                people that can rely on it to have a good idea of 
-                what\'s on your page.`,
+              name: 'caption',
+              type: 'string',
+              title: 'Caption',
               options: {
-                isHighlighted: true
+                isHighlighted: true // <-- make this field easily accessible
               }
+            },
+            {
+              // Editing this field will be hidden behind an "Edit"-button
+              name: 'attribution',
+              type: 'string',
+              title: 'Attribution',
+            }
+          ]
+        },
+      {
+          title: 'Created At',
+          name: 'createdAt',
+          type: 'datetime', 
+      },
+      {
+          name: 'author',
+          type: 'object',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'author'}]
             }
           ]
         }
-      ]
-    },
-    {
-      name: 'metadesc',
-      type: 'string',
-      title: 'Meta Description',
-    },
-    {
-      title: 'Blog Image',
-      name: 'blogimg',
-      type: 'image',
-      options: {
-        hotspot: true // <-- Defaults to false
-      },
-      fields: [
-        {
-          name: 'caption',
-          type: 'string',
-          title: 'Caption',
-          options: {
-            isHighlighted: true // <-- make this field easily accessible
-          }
-        },
-        {
-          // Editing this field will be hidden behind an "Edit"-button
-          name: 'attribution',
-          type: 'string',
-          title: 'Attribution',
-        }
-      ]
-    },
-
-    {
-        title: 'Created At',
-        name: 'createdAt',
-        type: 'datetime',
-      },
-      {
-        name: 'author',
-        type: 'object',
-        fields: [
-          {
-            title: 'Author',
-            name: 'author',
-            type: 'reference',
-            to: [{type: 'author'}]
-          }
-        ]
-      }
   ]
-};
+}
--- sanity-backend/schemas/schema.js
+++ sanity-backend/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import blog from './blog'
 import author from './author'
+import profile from './profile'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,6 +15,6 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    blog, author
+    blog, author, profile
   ]),
 })
